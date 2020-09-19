559347ac21f3999aa67494a25de9b3aa48738169
Jimmy <rockfiregunz@gmail.com>
2020-09-19 22:24:08 +0800

    非同步練習

--- SkImageResizer/SKImageProcess.cs
+++ SkImageResizer/SKImageProcess.cs
@@ -67,31 +67,7 @@ namespace SkImageResizer
             }
             await Task.WhenAll(Tasks);            
         }
-
-        public async Task  CreateImageFilesAsync(string filePath, string destPath, double scale)
-        {                            
-            var bitmap                  =   SKBitmap.Decode(filePath);
-            var imgPhoto                =   SKImage.FromBitmap(bitmap);        
-            var imgName                 =   Path.GetFileNameWithoutExtension(filePath);
-            var sourceWidth             =   imgPhoto.Width;
-            var sourceHeight            =   imgPhoto.Height;
-            var destinationWidth        =   (int)(sourceWidth * scale);
-            var destinationHeight       =   (int)(sourceHeight * scale);
-
-                await Task.Yield();  
-                using var scaledBitmap  =   bitmap.Resize(new SKImageInfo(destinationWidth, destinationHeight),SKFilterQuality.High);
-                using var scaledImage   =   SKImage.FromBitmap(scaledBitmap);   
-                
-                //await Task.Yield();
-                using var data          =   scaledImage.Encode(SKEncodedImageFormat.Jpeg, 100);
-                
-                //await Task.Yield();
-                using var s             =   File.OpenWrite(Path.Combine(destPath, imgName + ".jpg"));
-                data.SaveTo(s);
-                
-        } 
-
-
+ 
         public async Task  CreateImageFilesStreamAsync(string filePath, string destPath, double scale)
         {                         
             var bitmap                  =   SKBitmap.Decode(filePath);
